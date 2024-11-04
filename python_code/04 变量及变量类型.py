e0290c0c86fd6fac575b634319cd126c1c899152
Marks686 <17829791810@163.com>
2024-11-05 00:23:43 +0800

    变量以及变量类型

---
+++ python_code/04 变量及变量类型.py
@@ -0,0 +1,20 @@
+# 变量的初始化
+a1 = 100
+a2 = 200
+a3 = a1 + a2
+# print(a3)
+
+name = "鲁班"
+age = 18
+height = 1.55
+is_man = True
+
+# print(name,age,height,is_man)
+
+# 列表 list  元组 tuple 字典 dict
+
+# 使用type() 函数 来获得数据类型
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(is_man))
