776d3cbefd17c8cbb59aa349ff7b5cd7d5fd57bc
Marks686 <17829791810@163.com>
2024-11-05 00:48:32 +0800

    输入和输出

---
+++ python_code/06 输入和输出.py
@@ -0,0 +1,32 @@
+# print("zg")
+# print("z\ng")
+#
+# slogan = "我是战神"
+# print(slogan)
+# print("我的slogan是：", slogan)
+# print("我的slogan是：", 123)
+
+# 输入 常见的输入获取键盘输入内容
+# 使用函数 input() 用户以回车结束输入 输入内容被认为是字符串
+# print(1)
+# password = input()
+# print(2)
+# print(password)1
+# 推荐使用的格式 变量名 = input("提示信息")
+
+# password = input("请输入密码：")
+# print("刚才那个用户输入的密码是：",password)
+
+# 类型转换
+first_number = input("请输入第一个数字：")
+second_number = input("请输入第二个数字:：")
+result = int(first_number) + int(second_number)
+# print("计算结果是：", result)
+
+
+# 常用的类型转换函数
+int()
+float()
+str()
+bool()
+
