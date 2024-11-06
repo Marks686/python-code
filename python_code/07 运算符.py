a6ff624b1c798c074c9cd8d4573451088e37d335
Marks686 <17829791810@163.com>
2024-11-06 12:18:55 +0800

    运算符+判断语句

---
+++ python_code/07 运算符.py
@@ -0,0 +1,24 @@
+# 指数优先级最高然后就是乘除取整数取余...
+
+
+# print(1 - 2)
+# print(2 * 3)
+# print(8 / 2)
+# print(9 // 4)
+# print(9 % 4)
+# print(2 ** 3)
+# a = 19
+# a //= 5
+# print(a)
+#
+# a = 3
+# a **= 2
+# print(a)
+
+# b = 3
+# print(a != b)
+# print(3 == 3)
+
+# print(True and True)
+# print(True or False)
+print(not False)
