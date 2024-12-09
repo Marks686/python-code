ab9e7234ca6b0748bb15404b8aa4825c21e92eba
Marks686 <17829791810@163.com>
2024-12-09 21:24:23 +0800

    存储过程构造数据

---
+++ practice/test_2024_12_09/test.sql
@@ -0,0 +1,12 @@
+--mysql存储过程
+create procedured d () begin
+	declare
+	    i int default 1;
+    while
+        i <= 10000 do
+      insert into user
+      values
+        (i,concat("张三",i),13000000000+i,"123456");
+      set i = i+1;
+    end while
+end
